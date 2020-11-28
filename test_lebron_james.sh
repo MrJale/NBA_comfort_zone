1f86197834be43cf2401863005ad08399c9b62a8
Jia Guo <37137503+MrJale@users.noreply.github.com>
2020-11-28 15:27:59 -0500

    Create test_lebron_james.sh

---
+++ test_lebron_james.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source env.sh
+mvn clean package
+/usr/local/hadoop/bin/hdfs dfs -rm -r /kmeans/input/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /kmeans/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./shot_logs.csv /kmeans/input/
+/usr/local/spark/bin/spark-submit --class NBA_kmeans --master=spark://$SPARK_MASTER:7077 target/KMeans-1.0-SNAPSHOT.jar hdfs://$SPARK_MASTER:9000/kmeans/input/ "lebron james"
+mvn clean
